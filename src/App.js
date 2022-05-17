57f0bc1ac2901837c39c39042c6ecb5e635237dc
Zoran Bajic <zoranbajic@protonmail.com>
2022-05-16 23:02:25 -0500

    chore: finish cleaning up default files to begin working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from '@mui/material';
+import { Add, Settings } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant='text'>Text</Button>
+      <Button
+        startIcon={<Settings />}
+        variant='contained'
+        color='secondary'
+        size='small'
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant='contained'
+        color='success'
+        size='small'
+      >
+        Add New Post
+      </Button>
+      <Button variant='disabled'>Outlined</Button>
+      <Typography variant='h1' component='p'>
+        It uses h1 style but it's a p tag
+      </Typography>{' '}
     </div>
   );
 }
